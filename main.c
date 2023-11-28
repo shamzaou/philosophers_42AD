bd0bf4e6891fc249d828879fa851b06ad4d56886
shamzaou <shamzaou@student.42abudhabi.ae>
2023-11-29 03:53:24 +0400

    update

--- main.c
+++ main.c
@@ -3,26 +3,44 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: shamzaou <shamzaou@student.42abudhabi.a    +#+  +:+       +#+        */
+/*   By: shamzaou@student.42abudhabi.ae <shamzao    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/28 04:03:15 by shamzaou          #+#    #+#             */
-/*   Updated: 2023/11/28 05:34:43 by shamzaou         ###   ########.fr       */
+/*   Updated: 2023/11/29 03:53:06 by shamzaou@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-int main(int ac, char **av)
+int	main(int argc, char **argv)
 {
-    t_rules rules;
-    int     init_status;
+	t_rules rules;
+	int init_status;
 
-    if (ac != 5 && ac != 6)
-        return (display_error("Wrong number of Arguments!"));
-    init_status = init_all(&rules, av);
-    if (init_status)
-        return (error_handler(init_status));
-    if (philo(&rules))
-        return (display_error("Thread creation failure!"));
-    return (0);
+	if (argc != 5 && argc != 6)
+	{
+		printf("Usage: number_of_philosophers time_to_die time_to_eat time_to_sleep [number_of_times_each_philosopher_must_eat]\n");
+		exit(EXIT_FAILURE);
+	}
+	init_status = parse_and_initialise(argc, argv, &rules);
+	if (init_status != EXIT_SUCCESS)
+	{
+		printf("Error: Mutex initialisation failed!\n");
+        exit(EXIT_FAILURE);
+	}
+	if (philo(&rules))
+	{
+        printf("Error: Simulation failed!\n");
+        exit(EXIT_FAILURE);
+    }
+	return (EXIT_SUCCESS);
+}
+
+int parse_and_initialize(int argc, char **argv, t_rules *rules)
+{
+    if (init_all(rules, argv))
+    {
+        return (EXIT_FAILURE);
+    }
+    return (EXIT_SUCCESS);
 }
